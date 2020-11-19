4a58045e9599364f67be7be8141959db29e4b639
Ariel Xu <arielxu@live.com>
2020-11-19 13:36:29 +0800

    add the validations for the three models

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -1,4 +1,9 @@
 class Dose < ApplicationRecord
   belongs_to :cocktail
   belongs_to :ingredient
+  validates :description, presence: true
+
+# cannot have same pair of cocktail and ingredient for one dosage type
+# same as validates_uniqueness_of :cocktail, :scope => [:ingredient]
+  validates_uniqueness_of :ingredient, :scope => [:cocktail]
 end
